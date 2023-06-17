10706492e678755325147d1a7118095d1eec66da
anushkakakkar <91541334+anushkakakkar@users.noreply.github.com>
2023-06-17 12:55:42 +0530

    Add files via upload

---
+++ Library.js
@@ -0,0 +1,13 @@
+export let messsage = "Es6";
+
+export function user() {
+    console.log("Hello Anushka")
+
+}
+
+export class test {
+    constructor(){
+        console.log("Constructor method");
+    }
+
+}
---
+++ Main.js
@@ -0,0 +1,10 @@
+import {messsage} from "./Library.js";
+import {user} from "./Library.js";
+import {test} from "./Library.js";
+
+console.log(messsage);
+user();
+
+let a = new test();
+
+//document.body.innerHTML = messsage;
